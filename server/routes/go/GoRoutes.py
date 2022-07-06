b3ccf861970a2ae6344175ad0319d763d5c85d7d
hjoby <hugo.joby@gmail.com>
2022-07-06 14:46:18 -0400

    Error Handling

--- server/routes/go/GoRoutes.py
+++ server/routes/go/GoRoutes.py
@@ -1,19 +1,20 @@
 from flask import request
 
 from interface.go.GoPackage import GoPackage
+from logger.Logger import Logger
 from pull.GoPullEngine import GoPullEngine
 from server import app
 from server.interfaces.ApiResponse import ApiResponse
 from server.middlewares import token_required
 
 goPullEngine = GoPullEngine()
-
+__logger = Logger.get("GoRoutes")
 
 @app.route('/go/check', methods=['POST'])
 @token_required
 def go_check():
     """
-        Check the compatibility of a Go package
+       Check the compatibility of a Go package
     """
     # Process the request containing the go package
     data = request.json
@@ -21,11 +22,11 @@ def go_check():
     # Verify data sent
     if not "name" not in data:
         # Malformed request
-        return ApiResponse("Malformed package", None, ["Malformed request: Missing 'name' field."])
+        return ApiResponse("Malformed request", None, ["Missing 'name' field."]).build()
 
     if "version" not in data:
         # Malformed request
-        return ApiResponse("Malformed package", None, ["Malformed request: Missing 'version' field."])
+        return ApiResponse("Malformed request", None, ["Missing 'version' field."]).build()
 
     origin = ""
     if "origin" in data:
@@ -34,5 +35,7 @@ def go_check():
 
     package = GoPackage(data["name"], data["version"], str(origin))
     compatibility = goPullEngine.pull_package(package)
+
+    __logger.info(f"Package: {package.name} - Compatibility {compatibility.compatible}")
 
     return ApiResponse("Compatibility result", compatibility.serialize(), []).build()
