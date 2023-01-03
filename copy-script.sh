3a2b9bdf0b6000c436af26a1a06ca89ac86632dc
hjoby <hugo.joby@gmail.com>
2023-01-03 10:41:11 -0500

    Fix: Copy script

--- copy-script.sh
+++ copy-script.sh
@@ -5,8 +5,9 @@ if service --status-all | grep -Fq 'arm-cc-go'; then
   sudo service arm-cc-go stop
 fi
 
-sudo cp arm-cc-go.service /etc/systemd/arm-cc-go.service
+sudo cp arm-cc-go.service /etc/systemd/system/arm-cc-go.service
+sudo systemd-analyze verify arm-cc-go.service
 
-systemctl enable arm-cc-go
-systemctl daemon-reload
-systemctl start arm-cc-go
+sudo systemctl enable arm-cc-go
+sudo systemctl daemon-reload
+sudo systemctl start arm-cc-go
